2619de3c7c0434f350f8a3812788278e91a97362
fabianlee1211 <chleefabian@gmail.com>
2023-01-22 20:27:41 +0800

    fix: container and date style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         title="In Time Life Calendar"
         description="Visualize your remaining time of your life with the In Time Life Calendar. Make the most of every moment and never let another one slip away."
       />
-      <section className="max-w-screen-2xl px-6 lg:px-12 mx-auto w-full flex-1 flex flex-col items-center justify-center">
+      <section className="max-w-screen-2xl px-6 lg:px-12 mx-auto w-full flex-1 flex flex-col items-center justify-center py-16">
         <App />
       </section>
     </>
