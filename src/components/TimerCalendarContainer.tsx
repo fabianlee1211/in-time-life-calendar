0276a13584189cb93f39fcd92f093fe77edf669a
Fabian Lee <chleefabian@gmail.com>
2023-01-23 03:41:48 +0800

    feat: disclaimer text (#5)

--- src/components/TimerCalendarContainer.tsx
+++ src/components/TimerCalendarContainer.tsx
@@ -1,4 +1,3 @@
-import { useLocalStorage } from 'react-use';
 import { useRouter } from 'next/router';
 import Timer from './Timer';
 import { useCallback } from 'react';
@@ -24,6 +23,15 @@ export default function TimerCalendarContainer() {
     <>
       <Timer {...config} onReset={reset} />
       <Calendar {...config} />
+      <div className="max-w-2xl px-6 md:px-12 self-start text-xs opacity-30 mx-auto text-center">
+        <p className="font-medium">Disclaimer</p>
+        <p className="mt-2">
+          The calendar assumes every year has 52 weeks (7 x 52 = 364 Days) for
+          the ease of calculation. In reality we are missing approximately 1 day
+          in common years and 2 days in leap years. The purpose of this calendar
+          is not about 100% accuracy but to help you visualize your lifetime.
+        </p>
+      </div>
     </>
   );
 }
