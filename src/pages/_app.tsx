0597150d10da71f385ed6872d7dc668427abbf94
fabianlee1211 <chleefabian@gmail.com>
2023-01-23 01:53:36 +0800

    feat: add fathom analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import Layout from '@/components/Layout';
 import type { AppProps } from 'next/app';
-import { Analytics } from '@vercel/analytics/react';
+import useFathom from '@/useFathom';
 import 'react-datepicker/dist/react-datepicker.css';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useFathom();
+
   return (
     <Layout>
       <Component {...pageProps} />
-      <Analytics />
     </Layout>
   );
 }
